8bb3640a41d78b7fa90c1480078962ad32f9fcc4
phankons <stephen.han@gmx.com>
2023-08-08 10:18:07 -0400

    Create Mock Data, Write Helper Functions, Populate Helper Functions

--- src/components/MatchList.js
+++ src/components/MatchList.js
@@ -2,8 +2,9 @@
 
 import React from "react"; //optional
 import Match from "./Match";
+import matchData from "../data/matchData";
 
-function MatchList() {
+function MatchList(props) {
   return (
     <section className="PlayerList MatchList">
       <h1>Math list</h1>
--- src/components/PlayerList.js
+++ src/components/PlayerList.js
@@ -2,8 +2,15 @@
 
 import React from "react"; //optional
 import Player from "./Player";
+import playerData from '../data/playerData';
+import matchData from '../data/matchData';
+import {preparePlayerData, addWinsToPlayers} from '../helpers/playerHelper';
 
-function PlayerList() {
+function PlayerList(props) {
+  
+  const playerDataArray = preparePlayerData(playerData);
+  const parsedPlayerData = addWinsToPlayers(playerDataArray, matchData);
+
   return (
     <section className="PlayerList">
       <h1>Current participating players</h1>
