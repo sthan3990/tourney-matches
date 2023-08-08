c8939291d1d4375164d0cfddb1976a03c343ef2e
phankons <stephen.han@gmx.com>
2023-08-08 10:07:38 -0400

    Setup App.js to show MatchList and PlayerList

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import PlayerList from './components/PlayerList';
+import MatchList from './components/MatchList';
 
 function App() {
   return (
-    <div>
-      
+    <div className="App">
+      <h1>
+        Tourney Matches <span>Where Coding and Tournaments found their Match!</span>
+      </h1>
+      <PlayerList />
+      <MatchList />
     </div>
   );
 }
---
+++ src/components/MatchList.js
@@ -0,0 +1,15 @@
+// MatchList.js
+
+import React from "react"; //optional
+import Match from "./Match";
+
+function MatchList() {
+  return (
+    <section className="PlayerList MatchList">
+      <h1>Math list</h1>
+      <Match />
+    </section>
+  );
+}
+
+export default MatchList;
